e2336ae31f18ad4315e508d95c09c09708cd1040
Roman Zhigalkin <r.zhygalkin@gmail.com>
2019-05-04 12:51:47 +0300

    End switch verification, update train pulse funct

--- scanner-tst-2/Arduino/sketch_may02b/sketch_may02b.ino
+++ scanner-tst-2/Arduino/sketch_may02b/sketch_may02b.ino
@@ -8,12 +8,17 @@ int high, low;
 
 #define DUTY_CICLE 0.25
 
-#define DIRECTION_PIN 9
-#define HORIZONTAL_PIN 10
-#define VERTICAL_PIN 11
-#define PHEM_LEFT_PIN 12
-#define PHEM_RIGHT_PIN 13
+#define DIRECTION_PIN 12
 
+#define HORIZONTAL_PIN 5
+#define VERTICAL_PIN 6
+#define PHEM_LEFT_PIN 9
+#define PHEM_RIGHT_PIN 10
+
+#define END_SWITCH_UP 2
+#define END_SWITCH_DOWN 4
+#define END_SWITCH_LEFT 7
+#define END_SWITCH_RIGHT 8
 
 String getCommand () {
   if (inputString && inputString.length()) {
@@ -38,6 +43,21 @@ String getParam () {
   return "";
 }
 
+int getEndSwitch (String direction) {
+  int direction_state = digitalRead(DIRECTION_PIN);
+  if (direction.equals("VERTICAL")) {
+    if (direction_state > 0) {
+      return END_SWITCH_UP;
+    }
+    return END_SWITCH_DOWN;
+  }
+  
+  if (direction_state > 0) {
+    return END_SWITCH_LEFT;
+  }
+  return END_SWITCH_RIGHT;
+}
+
 void setSystemParams (String f) {
   frequency = f.toDouble();
   period = (1.0 / frequency) * 1000000.0;
@@ -52,10 +72,19 @@ void Pulse (int channel) {
   delayMicroseconds(low);
 }
 
-void TrainPulse (int channel, int count) {
+int TrainPulse (int channel, int count, int end_switch) {
+  int end_switch_state;
+  int steps_done = 0;
   for (int i = 0; i < count; i++) {
     Pulse(channel);
+    end_switch_state = digitalRead(end_switch);
+    steps_done += 1;
+    if (end_switch_state > 0) {
+      return steps_done;
+    }
   }
+
+  return steps_done;
 }
 
 void ToggleDirection (String direction) {
@@ -66,8 +95,15 @@ void ToggleDirection (String direction) {
   }
 }
 
+
+
 void setup () {
   Serial.begin(9600);
+  pinMode(END_SWITCH_UP, INPUT);
+  pinMode(END_SWITCH_DOWN, INPUT);
+  pinMode(END_SWITCH_LEFT, INPUT);
+  pinMode(END_SWITCH_RIGHT, INPUT);
+  
   pinMode(DIRECTION_PIN, OUTPUT);
   pinMode(HORIZONTAL_PIN, OUTPUT);
   pinMode(VERTICAL_PIN, OUTPUT);
@@ -91,17 +127,21 @@ void loop () {
       Serial.println(command + " " + param);
     } else if (command.equals("MOVE_HORIZONTAL")) {
       int steps = param.toInt();
-      TrainPulse(HORIZONTAL_PIN, steps);
-      Serial.println(command + " " + param);
+      int end_switch = getEndSwitch("HORIZONTAL");
+      int steps_done = TrainPulse(HORIZONTAL_PIN, steps, end_switch);
+      Serial.println("COMMAND = " + command + " PARAM = " + param + " STEPS_DONE = " + steps_done + " END_SWITCH = " + end_switch);
     } else if (command.equals("MOVE_VERTICAL")) {
       int steps = param.toInt();
-      TrainPulse(VERTICAL_PIN, steps);
-      Serial.println(command + " " + param);
+      int end_switch = getEndSwitch("VERTICAL");
+      int steps_done = TrainPulse(VERTICAL_PIN, steps, end_switch);
+      Serial.println("COMMAND = " + command + " PARAM = " + param + " STEPS_DONE = " + steps_done + " END_SWITCH = " + end_switch + " HIGHT = " + HIGH + " LOW = " + LOW);
     }
     
     inputString = "";
   }
 }
+
+///Test
 
 void serialEvent () {
   while (Serial.available()) {
